9faf24e042ce89e43f306a807c464dcf23443005
kocesat <kocesat@gmail.com>
2023-09-26 00:13:56 +0300

    refactor: ifNull utility

--- src/main/java/com/kocesat/mybatisdemo/util/AppUtil.java
+++ src/main/java/com/kocesat/mybatisdemo/util/AppUtil.java
@@ -3,10 +3,12 @@ package com.kocesat.mybatisdemo.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class AppUtil {
 
   public static <T> T ifNull(T obj, T defaultObj) {
-    return obj == null ? defaultObj : obj;
+    return Objects.requireNonNullElse(obj, defaultObj);
   }
 }
